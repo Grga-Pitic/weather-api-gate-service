ec96990c93bc3026bb7ac295616aedcfc776bec8
Ivan <ikon98@gmail.com>
2021-01-05 21:53:34 +0300

    Removes line breaks and import

--- src/main/java/com/pet/main/WeatherServiceApplication.java
+++ src/main/java/com/pet/main/WeatherServiceApplication.java
@@ -1,14 +1,12 @@
 package com.pet.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherServiceApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(WeatherServiceApplication.class, args);
 	}
 
